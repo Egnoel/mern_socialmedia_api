48db0c9c2c180dba9b8ddcfc64291d1dd59544af
Egnoel <egnoel@hotmail.com>
2023-06-06 10:00:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
+import { register } from './controllers/auth.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/assets');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/auth/register', upload.single('picture'), register);
+
+app.use('/auth', authRoutes);
+
+mongoose
+  .connect(`${process.env.MONGO_URI}/social`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
---
+++ models/User.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    picturePath: {
+      type: String,
+      default: '',
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    ocupation: String,
+    viewsdProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export default User;
